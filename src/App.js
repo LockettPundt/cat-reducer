5a88446f971256e562d255af9fbdca67d3528323
Lockett Pundt <kalifornium@hotmail.com>
2020-04-06 15:58:50 -0400

    deleted the boilerplate stuff and imported the activity compnent.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import './App.css';
-
+import Activity from './components/Activity'
 function App() {
   return (
     <div className="App">
-      
+      <Activity />
     </div>
   );
 }
