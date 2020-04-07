3314ad2c05a1d7bdf20dff454dce1cb95e7dfbd6
Lockett Pundt <kalifornium@hotmail.com>
2020-04-07 12:15:10 -0400

    imported the new update functions and placed useState hooks in the component function. created event handlers to control the form and to change the state values on submit.

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,32 +1,66 @@
-import React from 'react';
+/* eslint-disable no-shadow */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { eat, nap, play } from '../redux/actions';
 import styled from 'styled-components';
+import {
+  setName, setActivity,
+} from '../redux/actions';
 
 const Button = styled.button`
   background-color: salmon;
   padding: 0.6rem 1rem;
   font-size: 1rem;
-  margin: 0 2vw;
+  margin: 1vw 2vw;
   border: solid 2px rgb(23, 23, 23);
-`
-const Activity = ({ activity, eat, play, nap }) => {
-  
+`;
+
+
+const Activity = ({
+  activity, name, setName, setActivity,
+}) => {
+  const [inputActivity, setInputActivity] = useState(activity);
+  const [inputName, setInputName] = useState(name);
+
+  const handleName = (e) => {
+    setInputName(e.target.value);
+  };
+
+  const handleActivity = (e) => {
+    setInputActivity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName(inputName);
+    setActivity(inputActivity);
+    console.log('Submitted');
+  };
+
   return (
     <>
       <h1>What is the cat doing?</h1>
-      <p>The cat is.....{activity}</p>
-      <Button onClick={() => eat()}>Eat!</Button>
-      <Button onClick={() => play()}>Play!</Button>
-      <Button onClick={() =>  nap()}>Nap!</Button>
+      <p>
+        {name}
+        {' '}
+        is.....
+        {activity}
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input value={inputName} placeholder="Enter a name" onChange={handleName} />
+        <input value={inputActivity} placeholder="enter an activity" onChange={handleActivity} />
+        <Button type="submit">SUBMIT!</Button>
+      </form>
+
     </>
   );
-}
+};
 
-const mapStateToProps = state => {
-  const { activity } = state;
+
+const mapStateToProps = (state) => {
+  const { activity, name } = state;
   return activity;
-}
+};
 
 
-export default connect(mapStateToProps, { eat, play, nap })(Activity);          
+export default connect(mapStateToProps, { setName, setActivity })(Activity);
