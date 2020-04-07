a180c2ef8141946460fe9332911dd33656245ed1
Lockett Pundt <kalifornium@hotmail.com>
2020-04-07 10:06:07 -0400

    imported the cat actions from actions. created a mapstatetoprops function to get the activity state of the cat and passed it into the connect function. then passed each action in the dispatch area. added the activity and actions into the props of the component function to use as click handlers to update the activity state.

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { eat, nap, play } from '../redux/actions';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: salmon;
+  padding: 0.6rem 1rem;
+  font-size: 1rem;
+  margin: 0 2vw;
+  border: solid 2px rgb(23, 23, 23);
+`
+const Activity = ({ activity, eat, play, nap }) => {
+  
+  return (
+    <>
+      <h1>What is the cat doing?</h1>
+      <p>The cat is.....{activity}</p>
+      <Button onClick={() => eat()}>Eat!</Button>
+      <Button onClick={() => play()}>Play!</Button>
+      <Button onClick={() =>  nap()}>Nap!</Button>
+    </>
+  );
+}
+
+const mapStateToProps = state => {
+  const { activity } = state;
+  return activity;
+}
 
 
-const Activity = props => (
-  <h1>Activity Component!</h1>
-);
-
-
-
-export default Activity;
+export default connect(mapStateToProps, { eat, play, nap })(Activity);          
