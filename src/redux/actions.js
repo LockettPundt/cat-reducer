0f5965f5cba2d8ae48c97079d2560d0e74bd1536
Lockett Pundt <kalifornium@hotmail.com>
2020-04-07 21:29:37 -0400

    created a setNewCat functiont to deliver a payload of id, name and activity that aare passed as arguments.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,6 @@
 import {
-  ACTION_SET_ACTIVITY, ACTION_SET_NAME,
+  ACTION_SET_ACTIVITY, ACTION_SET_NAME, ACTION_ADD_CAT, ACTION_EAT, ACTION_NAP, ACTION_PLAY,
 } from './actionTypes';
-
 
 export const setName = (name) => ({
   type: ACTION_SET_NAME,
@@ -13,6 +12,31 @@ export const setName = (name) => ({
 export const setActivity = (activity) => ({
   type: ACTION_SET_ACTIVITY,
   payload: {
+    activity,
+  },
+});
+
+export const nap = () => ({
+  type: ACTION_NAP,
+});
+
+export const eat = () => ({
+  type: ACTION_EAT,
+  payload: {
+    activity: 'eating',
+  },
+});
+
+export const play = () => ({
+  type: ACTION_PLAY,
+});
+
+
+export const setNewCat = (id, name, activity) => ({
+  type: ACTION_ADD_CAT,
+  payload: {
+    id,
+    name,
     activity,
   },
 });
